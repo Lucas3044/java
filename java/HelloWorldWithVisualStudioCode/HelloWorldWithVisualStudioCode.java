639ec9e17e366a443632012c73c30a99303dfad1
Lucas <blackdoc84@gmail.com>
2023-01-31 22:56:07 +0900

    initial Create

--- java/HelloWorldWithVisualStudioCode/HelloWorldWithVisualStudioCode.java
+++ java/HelloWorldWithVisualStudioCode/HelloWorldWithVisualStudioCode.java
@@ -4,6 +4,6 @@
 public class HelloWorldWithVisualStudioCode {
 
     public static void main(String[] args) {
-        System.out.println("Hello World With Visual Studio Code");
+        System.out.println("Hello World With Visual Studio Code!!");
     }
 }
